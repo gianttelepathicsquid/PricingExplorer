2f99eec39a301e08a5165105fe39585740c18372
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-24 23:43:58 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import PricingExplorer from '@/components/PricingExplorer'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <PricingExplorer />
+    </main>
+  )
+}
